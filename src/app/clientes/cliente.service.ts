ff4c1b8aff97f16b8c69662e6074f5f0e31f9bc0
inizubia <inigozubia@gmail.com>
2021-12-29 17:36:49 +0100

    CRUD de clientes

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,20 +2,38 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import { of, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
 
-  private urlEndpont: string = 'http://localhost:8080/api/clientes';
+  private urlEndpoint: string = 'http://localhost:8080/api/clientes';
+
+  private httpHeader = new HttpHeaders({'Conten-Type': 'application/json'});
 
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
     //return of(CLIENTES);
-    return this.http.get(this.urlEndpont).pipe(
+    return this.http.get(this.urlEndpoint).pipe(
       map( (response) => response as Cliente[])
     );
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers: this.httpHeader});
+  }
+
+  getCliente(id): Observable<Cliente>{
+    return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`);
+  }
+
+  update(cliente: Cliente): Observable<Cliente>{
+    return this.http.put<Cliente>(`${this.urlEndpoint}/${cliente.id}`, cliente, {headers: this.httpHeader});
+  }
+
+  delete(id: number): Observable<Cliente>{
+    return this.http.delete<Cliente>(`${this.urlEndpoint}/${id}`, {headers: this.httpHeader})
   }
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -16,6 +17,32 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  public delete(cliente: Cliente): void{
+    swal.fire({
+      title: '¿Estas seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swal.fire(
+              'Cliente eliminado!',
+              `Cliente ${cliente.nombre} eliminado con exito`,
+              'success'
+            )
+          }
+        )
+      }
+    })
   }
 
 }
